48c800cf49e99e97059f9cb1a6164e3bfb2c5a1b
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-05-24 16:19:58 -0300

    🚧 feat: componentização inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import "./App.css";
+import { Card } from "./components/StyledComponents/Card/Card";
+import { Container } from "./components/StyledComponents/Container/Container";
 
 
 function App() {
-
   return (
     <body>
-      <h1>Hello World!</h1>
+      <Container>
+        <Card>
+          <h1>Hello World!</h1>
+        </Card>
+      </Container>
     </body>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/StyledComponents/Card/Card.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  background-color: var(--white);
+  border-radius: 20px;
+  padding: 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`
+
+export { Card };
