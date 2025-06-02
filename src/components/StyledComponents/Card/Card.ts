17ddb6f5d639cf875158359775126103557ea4e6
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-06-02 19:35:30 -0300

    💄 style: adicionando responsividade para o Card

--- src/components/StyledComponents/Card/Card.ts
+++ src/components/StyledComponents/Card/Card.ts
@@ -2,9 +2,16 @@ import styled from "styled-components";
 
 const Card = styled.div`
   background-color: var(--white);
-  border-radius: 20px;
-  padding: 20px;
+  padding: 40px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  width: 700px;
+  border-radius: 20px 20px 210px 20px;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 20px;
+    border-radius: 20px 20px 110px 20px;
+  }
 `
 
 export { Card };
